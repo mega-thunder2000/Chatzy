3d0ae0246002a330ba64935e4fb55accd0d7230e
root <mega.thunder2000@gmail.com>
2017-12-26 20:32:02 -0600

    Test
    test

---
+++ Chatzy/Setup.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Chatzy
+{
+    class Setup
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+            var driverService = PhantomJSDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            driver = new PhantomJSDriver(driverService);
+
+            var collection = Enumerable.Range(0, 1338);
+            foreach (var item in collection)
+            {
+                driver.Url = "http://collabedit.com/new"; driver.Navigate();
+                string page = driver.Url.Split('/').Last();
+                File.AppendAllText("Collabedit.txt", page + Environment.NewLine);
+                driver.Url = "www.bing.com"; driver.Navigate();
+            }
+            driver.Close();
+
+            var lines = File.ReadAllLines("Collabedit.txt");
+            Array.Sort(lines);
+            File.WriteAllLines("Collabedit.txt", lines);
+        }
+    }
+}
---
+++ LDAP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryEntry de = new DirectoryEntry("LDAP://ust-viscount.stthomas.edu/OU=Distribution Groups,DC=stthomas,DC=edu")
+            {
+                Username = "chou0015",
+                Password = "Stthomas1"
+            };
+
+            DirectorySearcher ds = new DirectorySearcher(de)
+            {
+                //Filter = "(&(pwdLastSet=*)(displayName=*))",
+                PageSize = int.MaxValue
+            }; 
+            //ds.PropertiesToLoad.AddRange(new string[] { "displayName", "cn" });
+            var groups = ds.FindAll();
+
+            foreach (SearchResult sr in groups)
+            {
+                //File.AppendAllText("UST_LDAP_GROUPS.csv", string.Format("{0},{1}@stthomas.edu\n", sr.Properties["displayName"][0].ToString(), sr.Properties["cn"][0].ToString()));
+            }
+        }
+    }
+}
